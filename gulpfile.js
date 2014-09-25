3b4b0367c201668a5546f9683d75421160f8c59e
kezhizhi <kezhizhi@163.com>
2014-09-25 18:26:02 +0800

    添加到插件

--- gulpfile.js
+++ gulpfile.js
@@ -1,36 +1,23 @@
 //引入插件
 var gulp = require('gulp');
-var connect = require('gulp-connect');
+var uglify = require('gulp-uglify');
+var rename = require('gulp-rename');
 
-//创建watch任务去检测文件的改动
-gulp.task('watch', function () {
-    gulp.watch(['./www/*.html'], ['html']);
-	gulp.watch(['./www/css/*.css'], ['css']);
-	gulp.watch(['./www/js/*.js'], ['js']);
+
+require('shelljs/global');
+gulp.task('clean',function(){
+	rm('-rf', 'dist');
 });
-/*gulp.task('watch', function () {
-	gulp.watch(['./www/css*//*.css'], ['css']);
-});
- gulp.task('watch', function () {
-	gulp.watch(['./www/js*//*.js'], ['js']);
-});*/
-//使用connect启动一个Web服务器
-gulp.task('connect', function () {
-    connect.server({
-        root: 'www',
-        livereload: true
-    });
-});
-//调用要执行的任务
-gulp.task('html', function () {
-    gulp.src('./www/*.html').pipe(connect.reload());
-});
-gulp.task('css', function () {
-	gulp.src('./www/css/*.css').pipe(connect.reload());
-});
-gulp.task('js', function () {
-	gulp.src('./www/js/*.js').pipe(connect.reload());
+
+gulp.task('uglify', function() {
+	gulp.src('src/*.js')
+		.pipe(uglify())
+		.pipe(rename(function (path) {
+			path.extname = ".min.js"
+		}))
+		.pipe(gulp.dest('dist'))
+		.pipe(gulp.dest('./example/www/js'))
 });
 
 //运行任务
-gulp.task('default', ['connect', 'watch']);
+gulp.task('default', ['clean','uglify']);
